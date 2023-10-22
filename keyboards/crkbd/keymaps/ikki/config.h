1835aef71aba8bccd83073a126eaf97902db9dd1
fjtezg <87853873+fjtezg@users.noreply.github.com>
2023-10-22 13:56:36 +0900

    Update config.h

--- keyboards/crkbd/keymaps/ikki/config.h
+++ keyboards/crkbd/keymaps/ikki/config.h
@@ -29,7 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+#define TAPPING_TERM 120
 #define RETRO_TAPPING
 
 #define HOLD_ON_OTHER_KEY_PRESS
